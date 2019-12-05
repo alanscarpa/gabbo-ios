6d59a27cc0e6c9d64c55fc48ea07ccb03f91adb4
Alan Scarpa <ascarpa@apple.com>
2019-12-04 23:48:18 -0800

    Connecting to Twilio

--- Gabbo/View Controllers/VideoChat/VideoChatViewController.swift
+++ Gabbo/View Controllers/VideoChat/VideoChatViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import TwilioVideo
 
 class VideoChatViewController: UIViewController {
+
+    var videoChatRoom: Room?
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -22,6 +25,34 @@ class VideoChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        connectToVideoChat()
     }
 
+    private func connectToVideoChat() {
+        let connectOptions = ConnectOptions(token: PrivateConstants.twilioAccessToken) { builder in
+            // Not needed at the moment but it could be nice to have in the future.
+            // I'm leaving this here as a reminder for now.
+            // TODO: If this is still around 6/1/2020, delete me!
+            // builder.roomName =
+        }
+        videoChatRoom = TwilioVideoSDK.connect(options: connectOptions, delegate: self)
+    }
+
+}
+
+extension VideoChatViewController: RoomDelegate {
+    func roomDidConnect(room: Room) {
+        print("Yay! We connected to \(room.name)")
+    }
+
+    func roomDidDisconnect(room: Room, error: Error?) {
+        print("Goodbye. Disconnecting from \(room.name)")
+        if let error = error {
+            print("Error on disconnect: \(error.localizedDescription)")
+        }
+    }
+
+    func roomDidFailToConnect(room: Room, error: Error) {
+        print("Oh no! Failed to connect with error: \(error.localizedDescription)")
+    }
 }
